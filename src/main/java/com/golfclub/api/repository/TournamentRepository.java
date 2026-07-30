85913737af6904fd928edbb9fbae9b8435e88dc1
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-29 23:52:30 -0230

    Update TournamentRepository.java

--- src/main/java/com/golfclub/api/repository/TournamentRepository.java
+++ src/main/java/com/golfclub/api/repository/TournamentRepository.java
@@ -1,1 +1,14 @@
+package com.golfclub.api.repository;
 
+import com.golfclub.api.model.Tournament;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface TournamentRepository extends JpaRepository<Tournament, Long> {
+
+    List<Tournament> findByStartDate(LocalDate startDate);
+
+    List<Tournament> findByLocationContainingIgnoreCase(String location);
+}
