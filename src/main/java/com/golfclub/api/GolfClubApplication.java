8ad49d9fab7743ba04e4a84b3b2fc279141a4121
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-29 23:53:06 -0230

    Update GolfClubApplication.java

--- src/main/java/com/golfclub/api/GolfClubApplication.java
+++ src/main/java/com/golfclub/api/GolfClubApplication.java
@@ -1,1 +1,12 @@
+package com.golfclub.api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfClubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GolfClubApiApplication.class, args);
+    }
+}
