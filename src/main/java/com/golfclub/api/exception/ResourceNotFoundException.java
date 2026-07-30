cf399e40d4f783459ea8fcb7811b0b47ed8758ca
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-29 23:51:16 -0230

    Update ResourceNotFoundException.java

--- src/main/java/com/golfclub/api/exception/ResourceNotFoundException.java
+++ src/main/java/com/golfclub/api/exception/ResourceNotFoundException.java
@@ -1,1 +1,7 @@
+package com.golfclub.api.exception;
 
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
